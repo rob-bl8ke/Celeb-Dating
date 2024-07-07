0abc8b2b6f578d1fee4bb044695a6524c9626a1c
Rob Blake <rob.bl8ke@outlook.com>
2024-07-07 14:01:45 +0200

    Register user with password, hash, and salt.

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> UserExists(string username) => await context.Users.AnyAsync(usr => usr.UserName.ToLower() == username.ToLower());
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,7 +2,7 @@
 
 public class AppUser
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public required string UserName { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
