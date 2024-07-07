0865c826af33d3394a29fccc235a98700b660895
Rob Blake <rob.bl8ke@outlook.com>
2024-07-07 14:26:37 +0200

    Do a basic login with the user (but uh-oh it returns the password and hash)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,5 +29,21 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(usr => usr.UserName == loginDto.Username.ToLower());
+        if (user is null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExists(string username) => await context.Users.AnyAsync(usr => usr.UserName.ToLower() == username.ToLower());
 }
