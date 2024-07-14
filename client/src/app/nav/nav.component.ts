a7bad447e0597e3eea3077f75257d5f78da26ad6
Rob Blake <rob.bl8ke@outlook.com>
2024-07-14 17:03:09 +0200

    Basic login UI component logic

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { inject } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn = false;
   model: any = {};
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 }
